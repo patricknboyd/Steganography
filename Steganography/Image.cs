c0b74ffc3871eadbd4345697cd008ee4bd21e33e
patricknboyd <patricknboyd@gmail.com>
2015-08-26 01:31:33 -0600

    First pass of message encoding code.
    The code to select the bit mask is not returning correct values yet.
    Still to be tweaked.

--- Steganography/Image.cs
+++ Steganography/Image.cs
@@ -1,7 +1,10 @@
 ﻿using ImageProcessor;
+using ImageProcessor.Imaging;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +13,9 @@ namespace Boyd.Steganography
 {
     public enum BitsToEncode
     {
-        Zero = 0,
         One = 1,
         Two = 2,
-        Three = 3,
         Four = 4,
-        Five = 5,
-        Six = 6,
-        Seven = 7,
         Eight = 8
     }
 
@@ -36,7 +34,8 @@ namespace Boyd.Steganography
                 case PixelFormat.Format16bppGrayScale:
                 case PixelFormat.Format16bppRgb555:
                 case PixelFormat.Format16bppRgb565:
-                    return 2;
+                    throw new NotImplementedException("2 bytes per pixel is not yet supported.");    
+                    //return 2;
 
                 case PixelFormat.Format24bppRgb:
                     return 3;
@@ -106,7 +105,14 @@ namespace Boyd.Steganography
         /// </summary>
         public int BytesPerPixel { get; private set; }
 
-        private ImageFactory ImageData;
+        /// <summary>
+        /// Gets whether a souce image has been loaded.
+        /// </summary>
+        public bool IsImageLoaded { get; private set; }
+
+        private ImageFactory SourceImage;
+        private int MessagePosition;
+        private bool EndOfMessageReached;
 
         public Image()
         {
@@ -115,10 +121,14 @@ namespace Boyd.Steganography
             this.BytesPerPixel = 0;
             this.CharacterEncoding = Encoding.UTF8;
             this.EncodedBits = BitsToEncode.One;
-            this.ImageData = null;
+            this.SourceImage = null;
             this.ImageDataLength = 0;
             this.ImagePixels = 0;
             this.PixelFormat = System.Drawing.Imaging.PixelFormat.DontCare;
+            this.IsImageLoaded = false;
+
+            this.MessagePosition = 0;
+            this.EndOfMessageReached = false;
         }
 
         /// <summary>
@@ -138,20 +148,172 @@ namespace Boyd.Steganography
         /// <param name="imageData">The image byte data.</param>
         public void LoadImage(byte[] imageData)
         {
-            ImageData = new ImageFactory();
-            ImageData.Load(imageData);
+            SourceImage = new ImageFactory();
+            SourceImage.Load(imageData);
 
-            PixelFormat = ImageData.Image.PixelFormat;
+            PixelFormat = SourceImage.Image.PixelFormat;
             BytesPerPixel = GetBytesPerPixel(PixelFormat);
 
-            ImagePixels = ImageData.Image.Width * ImageData.Image.Height;
+            ImagePixels = SourceImage.Image.Width * SourceImage.Image.Height;
             ImageDataLength = ImagePixels * BytesPerPixel;
 
 
             CalculateAvailableMessageBytes();
-            
+
+            IsImageLoaded = true;
         }
 
+        /// <summary>
+        /// Encodes a message into the loaded image.
+        /// </summary>
+        /// <param name="message"></param>
+        public void EncodeMessage(string message, Stream outStream)
+        {
+            if(!IsImageLoaded)
+            {
+                throw new InvalidOperationException("No source image has been loaded.");
+            }
+
+            byte[] messageBytes = CharacterEncoding.GetBytes(message);
+
+            if(messageBytes.Length > AvailableMessageBytes)
+            {
+                throw new InvalidOperationException(
+                    string.Format("Unable to fit message. Message is {0} bytes, and only {1} bytes are available.",
+                        messageBytes.Length,
+                        AvailableMessageBytes)
+                    );
+            }
+
+            MessagePosition = 0;
+            EndOfMessageReached = false;
+
+            using (FastBitmap bitmap = new FastBitmap(SourceImage.Image))
+            {
+                for(int y = 0; y < bitmap.Height; y++)
+                {
+                    for(int x = 0; x < bitmap.Width; x++)
+                    {
+                        WriteBytesToPixel(bitmap, messageBytes, x, y);
+
+                        if(EndOfMessageReached)
+                        {
+                            break;
+                        }
+                    }
+
+
+                    if (EndOfMessageReached)
+                    {
+                        break;
+                    }
+                }
+            }
+
+
+            SourceImage.Save(outStream);
+        }
+
+        private void WriteBytesToPixel(FastBitmap bitmap, byte[] messageBytes, int x, int y)
+        {
+            Color oldPixel = bitmap.GetPixel(x, y);
+
+            byte a, r, g, b;
+
+            switch (EncodedBits)
+            {
+                case BitsToEncode.One:
+                    byte valueMask;
+
+                    if (!EndOfMessageReached && GetNextMessageBitValue(messageBytes, MessagePosition, out valueMask))
+                    {
+                        r = (byte)(oldPixel.R & valueMask);
+                        MessagePosition += 1;
+                    }
+                    else
+                    {
+                        r = oldPixel.R;
+                        EndOfMessageReached = true;
+                    }
+
+                    if (!EndOfMessageReached && GetNextMessageBitValue(messageBytes, MessagePosition, out valueMask))
+                    {
+                        g = (byte)(oldPixel.R & valueMask);
+                        MessagePosition += 1;
+                    }
+                    else
+                    {
+                        g = oldPixel.G;
+                        EndOfMessageReached = true;
+                    }
+
+
+                    if (!EndOfMessageReached && GetNextMessageBitValue(messageBytes, MessagePosition, out valueMask))
+                    {
+                        b = (byte)(oldPixel.R & valueMask);
+                        MessagePosition += 1;
+                    }
+                    else
+                    {
+                        b = oldPixel.B;
+                        EndOfMessageReached = true;
+                    }
+
+                    if (BytesPerPixel == 4 && !EndOfMessageReached && GetNextMessageBitValue(messageBytes, MessagePosition, out valueMask))
+                    {
+                        a = (byte)(oldPixel.R & valueMask);
+                        MessagePosition += 1;
+                    }
+                    else
+                    {
+                        a = oldPixel.A;
+                        EndOfMessageReached = true;
+                    }
+
+                    break;
+
+                case BitsToEncode.Two:
+                case BitsToEncode.Four:
+                case BitsToEncode.Eight:
+                    throw new NotImplementedException();
+                default:
+                    throw new InvalidOperationException(string.Format("Unrecognized value for BitsToEncode enum: {0}.", EncodedBits.ToString()));
+            }
+
+            Color newPixel;
+
+            if(BytesPerPixel == 4)
+            {
+                newPixel = Color.FromArgb(a, r, g, b);
+            }
+            else
+            {
+                newPixel = Color.FromArgb(r, g, b);
+            }
+
+            bitmap.SetPixel(x, y, newPixel);
+
+        }
+
+        private bool GetNextMessageBitValue(byte[] messageBytes, int position, out byte valueMask)
+        {
+            int index = position / 8;
+
+            if(index < messageBytes.Length)
+            {
+                int offset = position % 8;
+
+                byte mask = (byte)(0x01 << (7 - offset));
+                valueMask = (byte)(((messageBytes[index] & mask) >> offset) | 0xFE);
+
+                return true;
+            }
+            else
+            {
+                valueMask = 0;
+                return false;
+            }
+        }
 
         private void CalculateAvailableMessageBytes()
         {
@@ -172,9 +334,9 @@ namespace Boyd.Steganography
         {
             if(disposing)
             {
-                if(ImageData != null)
+                if(SourceImage != null)
                 {
-                    ImageData.Dispose();
+                    SourceImage.Dispose();
                 }
             }
         }
